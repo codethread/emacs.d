767ae9fb6563083aa18b2364ec2799815085411a
Adam Hall <adamhalldesigns@gmail.com>
2022-08-21 20:15:56 +0100

    the great schism

--- .config/nvim/lua/codethread/lsp/settings/bashls.lua
+++
@@ -1,6 +0,0 @@
-return {
-	on_attach = function(client, bufnr)
-		require('codethread.lsp.settings.shared').lsp_highlight_document(client)
-		require('codethread.lsp.settings.shared').lsp_keymaps(bufnr)
-	end,
-}
